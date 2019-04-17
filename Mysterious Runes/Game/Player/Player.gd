142899e002eef6a235a54496589607b368f61095
MairaBatistta <mairabatistta@outlook.com>
2019-04-16 23:07:56 -0300

    Salto
    Arreglo del salto y disparo

--- Mysterious Runes/Game/Player/Player.gd
+++ Mysterious Runes/Game/Player/Player.gd
@@ -1,42 +1,67 @@
 extends KinematicBody2D
 
 export var speed = 250;
+export var gravity = 900;
+export var jump_speed = 450;
+export var shooting = false
+export var jumping = false
 
 var animation = "Idle";
-
 var velocity = Vector2()
 
-const GRAVITY = Vector2(0, 900)
-
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	shooting = false
+	jumping = false
+	
 	pass
 
 func _physics_process(delta):
-	velocity += delta * GRAVITY
+	_move(delta)
 	
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -1 * speed;
+	_shoot()
 	
-	if Input.is_action_pressed("move_right"):
-		velocity.x = 1 * speed;
+	_animate()
+
+func _move(delta):
+	velocity.y += delta * gravity
 	
-	velocity.x = lerp(velocity.x, 0.0, 0.3)
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	var sideX = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
-	animate()
+	velocity.x = (sideX * (speed * delta)) / delta
 	
-func animate():
-	if (animation != $Sprite/AnimationPlayer.current_animation):
+	if is_on_floor():
+		velocity.y = 0
+		jumping = false
+		if Input.is_action_pressed("jump"):
+			velocity.y = -jump_speed
+			jumping = true
+	
+	if shooting && !jumping: velocity.x = 0
+	move_and_slide(velocity, Vector2(0, -1))
+	
+func _shoot():
+	if !shooting:
+		if Input.is_action_pressed("shoot"):
+			shooting = true
+	
+
+func _animate():
+	if velocity.x != 0:
+		animation = "Walk"
+		$Sprite.scale.x = 0.5 if velocity.x > 0 else -0.5
+	else: animation = "Idle"
+	
+	if jumping: 
+		animation = "Jump"
+		if velocity.y > 0:
+			animation += " End"
+	
+	if shooting:
+		animation = "Shoot"
+		if jumping:
+			animation += " Jump"
+	
+	if animation != $Sprite/AnimationPlayer.current_animation:
 		$Sprite/AnimationPlayer.play(animation)
 	
-	if velocity.x == 0:
-		animation = "Idle"
-	else:
-		animation = "Walk"
-	
-	if velocity.x > 0:
-		$Sprite.scale.x = 0.5;
-	elif velocity.x < 0: 
-		$Sprite.scale.x = -0.5;
+	print(animation)
